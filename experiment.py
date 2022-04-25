cc03b70edf95ac5c66411acaecebeb7b8756f6a3
Kai-Siang Wang <bruce1198@gmail.com>
2022-04-25 11:47:28 +0800

    wip: different processor experiment

--- experiment.py
+++ experiment.py
@@ -1,17 +1,18 @@
 import sys
 sys.path.insert(0, 'src')
-
+from graph.dag import DAG
+import copy
 import matplotlib
 import matplotlib.pyplot as plt
 import argparse
 from openpyxl import Workbook
 import json
 import os
-from graph.dag import DAG
 from lib.generator import plot_DAG, workflows_generator
 import uuid
 import numpy as np
 import random
+
 # matplotlib.use('pdf')
 # matplotlib.rcParams.update({
 #     "pgf.texsystem": "pdflatex",
@@ -26,11 +27,11 @@ args = parser.parse_args()
 
 
 # Generate 100 graphs
-num_of_graph = 10
+num_of_graph = 1
 depths = [0, 1, 2]
 
 random_id = str(uuid.uuid4())[:8]
-folder = f'{args.mode}.{num_of_graph}.{args.strategy}.{random_id}'
+folder = f'processor.{args.mode}.{num_of_graph}.{args.strategy}.{random_id}'
 
 if not os.path.exists(folder):
     os.mkdir(folder)
@@ -41,81 +42,65 @@ if not os.path.exists(f'{folder}/dags'):
 if not os.path.exists(f'{folder}/log'):
     os.mkdir(f'{folder}/log')
 
+processors = [3, 4, 5, 6, 7]
+
 
 def generate_graph():
     print(f'Generating {num_of_graph} graphs...')
 
-    def worker(graph):
-        dag, edges, pos = workflows_generator(mode=args.mode)
+    def worker(graph, processor):
+        dag, edges, pos = workflows_generator(
+            mode=args.mode, processor=processor)
         plot_DAG(edges, pos, f'{folder}/dags/dag.{graph}.png')
-        with open(f'{folder}/samples/exp.'+str(graph)+'.json', 'w') as f:
+        with open(f'{folder}/samples/exp.{graph}.{processor}.json', 'w') as f:
             json.dump(dag, f, indent=4)
-        print(f'Graph {graph:>2}... Done')
 
     for graph in range(num_of_graph):
-        worker(graph)
+        for processor in processors:
+            worker(graph, processor)
 
 
 def run_experiment():
     fig, ax = plt.subplots()
-    with open(f'{folder}/log/result.log', 'w') as log, open(f'{folder}/log/error.log', 'w') as error_log:
-        wb = Workbook()
-        ws = wb.active
-        ws.title = f'{num_of_graph} Random Graphs'
-        ws.column_dimensions['A'].width = 15
 
-        def worker(graph):
-            print(f'Run graph {graph}...')
-            ws.append([f'Graph {graph}'])
-            log.write(f'================ Graph {graph} ================\n')
-            dag = DAG()
-            dag.read_input(f'{folder}/samples/exp.'+str(graph)+'.json')
-            _, makespan_origin, usage = dag.schedule('heft')
-            log.write(f'HEFT ORIGIN: {makespan_origin}, MEMEORY: {usage}\n')
-            ws.append(['ORIGIN', makespan_origin, usage])
-            log.write(f'================= HEFT LOOKUP =================\n')
-            memory_sizes = [usage - 10*i for i in range(20)]
-            ws.append(['Depth/Memory']+memory_sizes)
+    data = [0 for _ in processors]
 
-            data = []
+    def worker(graph, processor, pid):
+        print(f'Run with {processor} processors...')
+        dag = DAG()
+        dag.read_input(f'{folder}/samples/exp.{graph}.{processor}.json')
+        _, _, usage = dag.schedule('heft')
+        min_memory = usage
+        limit = usage
+        while True:
+            ok = False
             for depth in depths:
-                depth_row = [depth]
-                for memory_size in memory_sizes:
-                    log.write(f'DEPTH: {depth} MEMORY: {memory_size}\n')
-                    try:
-                        _, makespan = dag.schedule(
-                            'heft_lookup', memory_size, {"depth": depth, "strategy": args.strategy, "plot": False})
-                        log.write(f'{makespan}\n')
-                        depth_row.append(makespan)
-                    except Exception as e:
-                        depth_row.append('N/A')
-                        error_log.write(f'{e}\n')
-                data.append(depth_row[1:])
-                ws.append(depth_row)
-            data = np.array(data)
-            data = data.T.tolist()
-            data = list(map(lambda mems:
-                            min(list(map(lambda mem: sys.maxsize if mem == 'N/A' else int(mem), mems))), data))
+                # print(usage, limit)
+                try:
+                    _, _, memory = dag.schedule(
+                        'heft_lookup', limit, {"depth": depth, "strategy": args.strategy, "plot": False})
+                    min_memory = min(min_memory, memory)
+                    ok = True
+                except Exception:
+                    pass
+            limit -= 10
+            if not ok:
+                break
+        # print(min_memory, usage)
+        data[pid] += (usage - min_memory) / usage
+        # ax.bar(id, min_memory, color='b', width=0.25, label='lookup')
+        # ax.bar(id+0.25, usage, color='g', width=0.25, label='heft')
 
-            x = []
-            y = []
-            for i, data in enumerate(data):
-                if data > 1000:
-                    continue
-                x.append((data - makespan_origin) / makespan_origin)
-                y.append((-10*i) / usage)
+    for graph in range(num_of_graph):
+        for pid, processor in enumerate(processors):
+            worker(graph, processor, pid)
 
-            color = "#"+''.join([random.choice('0123456789ABCDEF')
-                                for _ in range(6)])
-            ax.plot(x, y, '--o', color=color)
-
-        for graph in range(num_of_graph):
-            worker(graph)
-        wb.save(f'{folder}/result.xlsx')
-    ax.set_title("Memory-Makespan")
-    ax.set_xlabel('Increased Makespan')
-    ax.set_ylabel('Reduced Memory')
-    # ax.set_ylim(-1, 0)
+    print(list(map(lambda d: d/num_of_graph, data)))
+    ax.bar(processors, data, label='Memeory reduction')
+    ax.legend()
+    ax.set_title("Memory-Processor")
+    ax.set_xlabel('# Processors')
+    ax.set_ylabel('Memory Usage')
     fig.savefig(f'{folder}/exp.png')
     plt.close()
 
